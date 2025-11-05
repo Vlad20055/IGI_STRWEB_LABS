3ef35d94192732ab82e029d3192fe6567a02d66b
Vlad20055 <maksimenkovvlad111@mail.ru>
2025-11-05 21:34:46 +0300

    Start css (complete some pages and requirements)

--- IGI/LR5/ComputerServiceApp/ComputerServiceApp/settings.py
+++ IGI/LR5/ComputerServiceApp/ComputerServiceApp/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # если папка static в корне проекта
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # для collectstatic
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
